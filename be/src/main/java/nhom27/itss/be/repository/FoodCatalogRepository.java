ac2826092ab1d2867e4b996d7e19fe66d7283e5b
Trần Mạnh Hùng <166214532+hosobaomat@users.noreply.github.com>
2025-06-14 23:00:09 +0700

    Move BE and FE to unified structure

--- be/src/main/java/nhom27/itss/be/repository/FoodCatalogRepository.java
+++
@@ -1,9 +0,0 @@
-package nhom27.itss.be.repository;
-
-import nhom27.itss.be.entity.FoodCatalog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface FoodCatalogRepository extends JpaRepository<FoodCatalog, Integer>, JpaSpecificationExecutor<FoodCatalog> {
-
-}
